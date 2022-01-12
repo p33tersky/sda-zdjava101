176cf5f6c330ca23b4f251194255e2c80ee4e4b6
Piotr <petervvlostek@gmail.com>
2022-01-12 21:15:06 +0100

    moja impl mapowania wg miasta

--- adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
+++ adv/school/src/main/java/pl/sdacademy/java/adv/school/domain/student/StudentService.java
@@ -4,6 +4,8 @@ import pl.sdacademy.java.adv.school.domain.student.model.Student;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StudentService {
     private final StudentRepository studentRepository;
@@ -24,4 +26,34 @@ public class StudentService {
         return lista;
 
     }
+
+    public List<Student> getStudentsSortedByAge() {
+
+        List<Student> allStudentsAge = studentRepository.findAllStudents();
+
+        return allStudentsAge.stream()
+                .sorted(Comparator.comparing(Student::getBirthDate))
+                .collect(Collectors.toUnmodifiableList());
+
+    }
+
+    public List<Student> getStudentsSortedByAgeDesc() {
+        List<Student> allStudentsAge = studentRepository.findAllStudents();
+
+        return allStudentsAge.stream()
+                .sorted(Comparator.comparing(Student::getBirthDate).reversed()).toList();
+    }
+
+    public Map<String, List<Student>> getStudentsGroupedByCityAndSortedByName() {
+
+        return studentRepository.findAllStudents().stream()
+                .sorted(Comparator.comparing(Student::getLastName).thenComparing(Student::getFirstName))
+                .collect(Collectors.groupingBy(Student::getCity));
+    }
+
+    public Map<String, Student> getStudentsMappedByIdentifier() {
+        return studentRepository.findAllStudents().stream()
+                .collect(Collectors.toMap(Student::getId, student -> student) );
+    }
+
 }
